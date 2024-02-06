dac7fab5c602f49f70c4aa3b6361f5fed8e31fc2
SrPabaum <mrmster6@gmail.com>
2024-02-06 00:27:49 -0300

    update for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import './App.css'
-import AdminButton from './components/AdminButton'
-import Agendamento from './components/Agendamentos'
+import './App.css';
+import Agendamento from './components/Agendamentos';
 
 function App() {
   return (
     <>
       <div>
-        <div className='adminbar'
-              style={{position: 'absolute', left: '5%', top:'10%'}}>
-          <AdminButton/>
-        </div>
         <h2>FAST BARBER</h2>
         <Agendamento/>
       </div>
@@ -18,4 +13,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
